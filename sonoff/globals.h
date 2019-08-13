861c22389c7363d276c9eb1649cb1fa07de78cbd
Lukasz Worsztynowicz <lukasz.worsztynowicz@altimi.com>
2019-08-13 02:39:12 +0200

    PWM with nice dimming

---
+++ sonoff/globals.h
@@ -0,0 +1,7 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define TIMER_PERIOD_NODEMCU    80*5000
+uint16_t PWM_VALUES_TAB[5][4]; // [PIN | TARGET_VALUE | CURRENT_VALUE | IS_READY]
+
+#endif /* end of include guard: GLOBALS_H */
